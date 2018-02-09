f29a031050f8e0a1a99494359f23ec8fe5f302cd
hmerciol <li.henri@merciol.fr>
2018-02-09 11:35:22 +0100

    TP02

---
+++ 01-rappels-es5/02.tableau.js
@@ -0,0 +1,23 @@
+villes = ["nantes","paris","saint-nazaire","angers","le mans"];
+
+villes.forEach(function(element){
+    console.log(element);
+});
+
+console.log("lettreADansToutesLesVilles == "+villes.every(function(element){
+    return element.includes("a");
+}));
+
+console.log("auMoinsUneVilleAvecUnTiret == "+villes.some(function(element){
+    return element.includes("-");
+}));
+
+console.log("villesSansTiretSansEspace == "+villes.filter(function(element){
+    return !(element.includes("-") || element.includes(" "));
+}));
+
+console.log("villesMajusculeSeTerminantParS == "+villes.filter(function(element){
+    return element.endsWith("s");
+}).map(function(element){
+    return element.toUpperCase();
+}));
